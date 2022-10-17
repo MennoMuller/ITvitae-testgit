508a7adb2718d4329b24212d4e5bfb92f087b2d2
MennoMuller <menno.muller.nl@gmail.com>
2022-10-17 11:32:19 +0200

    Schaakbord update: afmeting variabele

--- schaakbordArrays/src/com/mennomuller/Main.java
+++ schaakbordArrays/src/com/mennomuller/Main.java
@@ -3,9 +3,10 @@ package com.mennomuller;
 public class Main {
 
     public static void main(String[] args) {
-        String[][] schaakbord = new String[8][8];
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        int afmeting = 8;
+        String[][] schaakbord = new String[afmeting][afmeting];
+        for (int i = 0; i < afmeting; i++) {
+            for (int j = 0; j < afmeting; j++) {
                 schaakbord[i][j] = Character.toString('A' + i) + (j + 1);
                 System.out.println(schaakbord[i][j]);
             }
