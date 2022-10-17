4f0a87acdd09ce69c9863e379a9f9a220a4d22eb
MennoMuller <menno.muller.nl@gmail.com>
2022-10-17 12:01:39 +0200

    palindroom update: 3 regels code ipv 4, 1 variabele minder.

--- palindroomStringBuilder/src/com/mennomuller/Main.java
+++ palindroomStringBuilder/src/com/mennomuller/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String str = input.nextLine();
-        StringBuilder woord = new StringBuilder(str.toLowerCase());
-        System.out.println(str.equals(woord.reverse().toString()));
+        StringBuilder woord = new StringBuilder(input.nextLine().toLowerCase());
+        System.out.println(woord.toString().equals(woord.reverse().toString()));
     }
 }
