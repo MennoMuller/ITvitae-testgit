3db52266b68565bff6f0e080d64998dbeb3eef24
MennoMuller <menno.muller.nl@gmail.com>
2022-10-17 11:18:52 +0200

    Schaakbord update

--- schaakbordArrays/src/com/mennomuller/Main.java
+++ schaakbordArrays/src/com/mennomuller/Main.java
@@ -3,14 +3,12 @@ package com.mennomuller;
 public class Main {
 
     public static void main(String[] args) {
-	String[][] schaakbord = new String[8][8];
-    for(int i=0;i<8;i++){
-        for(int j=0;j<8;j++){
-            schaakbord[i][j]=Character.toString('A'+i)+(j+1);
-            System.out.println(schaakbord[i][j]);
+        String[][] schaakbord = new String[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                schaakbord[i][j] = Character.toString('A' + i) + (j + 1);
+                System.out.println(schaakbord[i][j]);
+            }
         }
-    }
-
-
     }
 }
