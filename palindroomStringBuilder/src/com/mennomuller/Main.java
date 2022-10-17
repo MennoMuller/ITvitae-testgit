d0318cf09872b5e4c40d76159f23fa705f1625af
MennoMuller <menno.muller.nl@gmail.com>
2022-10-17 12:18:44 +0200

    palindroom update: Mooiere output

--- palindroomStringBuilder/src/com/mennomuller/Main.java
+++ palindroomStringBuilder/src/com/mennomuller/Main.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        StringBuilder woord = new StringBuilder(input.nextLine().toLowerCase());
-        System.out.println(woord.toString().equals(woord.reverse().toString()));
+        StringBuilder woord = new StringBuilder(input.nextLine());
+        boolean isPalindroom = woord.toString().equalsIgnoreCase(woord.reverse().toString());
+        System.out.println(woord.reverse() + " is " + (isPalindroom ? "" : "g") + "een palindroom!");
+
     }
 }
